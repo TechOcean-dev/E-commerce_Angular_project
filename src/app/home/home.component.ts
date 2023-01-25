aea2058f3105e3a5a6bcaff8b08fcafba7aac56c
MALIK AKMAL Naseer <akmal.bsit352@iiu.edu.pk>
2023-01-25 21:23:45 +0500

    add main slider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgIf } from '@angular/common';
+import { ProductService } from '../servers/product.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+	images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/800/400`);
+  img = './assets/E2.jpg'
+  constructor(private product:ProductService) { }
 
   ngOnInit(): void {
+    // this.product.popularProduct().subscribe((result)=>{
+
+    // })
   }
+  
 
 }
--- src/app/servers/product.service.ts
+++ src/app/servers/product.service.ts
@@ -33,4 +33,7 @@ export class ProductService {
     return this.http.patch<product>(`http://127.0.0.1:8000/Productdelete/${product.id}/`, product)
 
   }
+  popularProduct(product:product){
+    return this.http.get<product>('http://127.0.0.1:8000/ProductList?_limit=3/')
+  }
 }
