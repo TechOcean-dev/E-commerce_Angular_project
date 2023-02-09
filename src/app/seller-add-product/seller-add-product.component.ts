2ce42d8123cd354a251f2c6c1d7928be938874fe
MALIK AKMAL Naseer <akmal.bsit352@iiu.edu.pk>
2023-02-09 22:51:04 +0500

    add User Login& registration form validations and default value

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ProductService } from '../servers/product.service';
 
 @Component({
@@ -10,18 +10,23 @@ export class SellerAddProductComponent implements OnInit {
   Selleremail: string = ''
   ProductAlert: boolean = false
   Product_category: any
-  PCategory(e:any){
-    console.log(e.target.value)
-  }
+  SellerId: any;
   constructor(private product: ProductService) { }
 
   ngOnInit(): void {
     this.product.product_category().subscribe((data)=>{
-      console.warn(data)
       this.Product_category= data
     })
+    if(localStorage.getItem('seller')){
+      let sellerstore = localStorage.getItem('seller')
+      let sellerdata = sellerstore && JSON.parse(sellerstore)[0]
+      this.SellerId = sellerdata.id
+    
+    }
+
   }
-  
+
+
   addProduct(data: any) {
     console.warn(data)
     this.product.addProduct(data).subscribe((result)=>{
